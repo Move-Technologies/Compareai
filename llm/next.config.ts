ac7db064b3d2fc1ffab4b8e6a9a97164b8d116bd
cloud443 <121417859+cloud443@users.noreply.github.com>
2024-11-25 21:31:56 +0530

    Update next.config.ts

--- llm/next.config.ts
+++ llm/next.config.ts
@@ -7,25 +7,27 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  distDir: 'build', // Specifies the output directory as "build"
   async rewrites() {
     return [
-        {
-            source: '/api/:path*',
-            destination: 'https://3.227.241.228/:path*' // Now using HTTPS
-        }
-    ]
-},
-webpack: (config, { isServer }) => {
-  if (!isServer) {
+      {
+        source: '/api/:path*',
+        destination: 'https://3.227.241.228/:path*', // Now using HTTPS
+      },
+    ];
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
       config.resolve.fallback = {
-          ...config.resolve.fallback,
-          net: false,
-          tls: false,
-          fs: false,
+        ...config.resolve.fallback,
+        net: false,
+        tls: false,
+        fs: false,
       };
-  }
-  return config;
-},
+    }
+    return config;
+  },
+  output: 'export', // Specifies export mode
 };
 
 export default nextConfig;
